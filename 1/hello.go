6123fa1a10a687afe5015555fd360fb8d02bab8d
Eduardo de Valle <jsvalle50@gmail.com>
2025-09-30 01:55:36 -0400

    Implementacion de un queue para la tarea 1

--- 1/hello.go
+++ 1/hello.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 )
 
@@ -29,6 +30,52 @@ func stack() {
 	fmt.Println("Pop:", stack)
 }
 
+// No se puede imprimir el contenido de la queue
+// directamente con fmt.Println(queue) porque no
+// imprime el contenido, imprime un puntero
+func printQueue(queue *list.List) {
+	for e := queue.Front(); e != nil; e = e.Next() {
+		fmt.Printf("%v ", e.Value)
+	}
+	fmt.Println()
+}
+
+func queue() {
+	queue := list.New()
+
+	fmt.Println("Inicial: ")
+	printQueue(queue)
+
+	queue.PushBack(10)
+	fmt.Println("Enqueue 10: ")
+	printQueue(queue)
+
+	queue.PushBack(20)
+	fmt.Println("Enqueue 20: ")
+	printQueue(queue)
+
+	queue.PushBack(30)
+	fmt.Println("Enqueue 30: ")
+	printQueue(queue)
+
+	frontElement := queue.Front()
+	fmt.Println("Peek:", frontElement.Value)
+
+	fmt.Println("Queue actual: ")
+	printQueue(queue)
+
+	queue.Remove(queue.Front())
+	fmt.Println("Dequeue: ")
+	printQueue(queue)
+	
+	queue.Remove(queue.Front())
+	fmt.Println("Dequeue: ")
+}
+
 func main() {
 	stack()
+
+	fmt.Println("")
+
+	queue()
 }
